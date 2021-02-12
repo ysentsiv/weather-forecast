17b0847c480d7587660bdc44d51d4b080f3713e0
Yurii Sentsiv <yurii.sentsiv@gmail.com>
2021-02-12 13:45:38 -0500

    Adding more emuns

--- src/enums/index.js
+++ src/enums/index.js
@@ -3,3 +3,13 @@ export const CityID  = {
     Ottawa: 6094817,
     Tokyo: 1850147
 }
+
+export const Units  = {
+    F: 'imperial',
+    C: 'metric'
+}
+
+export const DataType  = {
+    Weather: 'weather',
+    Forecast: 'forecast'
+}
