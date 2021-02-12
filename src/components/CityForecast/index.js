8f5551b9b04250273a864e156baa297bfba55239
Yurii Sentsiv <yurii.sentsiv@gmail.com>
2021-02-12 15:47:11 -0500

    Functional table

--- src/components/CityForecast/index.js
+++ src/components/CityForecast/index.js
@@ -1,10 +1,61 @@
-import React, { Fragment } from "react";
+import React, {useState} from "react";
+import Table from 'react-bootstrap/Table';
+import Pagination from 'react-bootstrap/Pagination';
+import './styles.scss';
+import { groupBy } from 'lodash';
 
 const CityForecast = (forecastData) => {
+    // helper to get day
+    const forecastDay = item => new Date(item.dt*1000).getDate();
+    const dataList = forecastData.forecastData.list;
+    const dayGroupedDataList = groupBy(dataList, forecastDay);
+    const [currentDayForecast, setCurrentDayForecast] = useState(dayGroupedDataList[Object.keys(dayGroupedDataList)[0]]);
+
+    console.log(currentDayForecast);
+    const paginationSize = 6;
+
+    const handleDayForecast = (day) => {
+        setCurrentDayForecast(dayGroupedDataList[day])
+    }
+
+    let items = [];
+    for (let number = 0; number < paginationSize; number++) {
+        const forecastDay = Object.keys(dayGroupedDataList)[number];
+        items.push(
+            <Pagination.Item key={number} onClick={() => handleDayForecast(forecastDay)}>
+                {forecastDay}
+            </Pagination.Item>,
+        );
+    }
+
     return (
-        <Fragment>
-            {/* {forecastData ? console.log(forecastData) : ''} */}
-        </Fragment>
+        <div className='forecastTable'>
+            <Table striped="true" bordered="true" hover="true" >
+                <thead>
+                    <tr>
+                        <th colSpan="2">Date</th>
+                        <th>Temp</th>
+                        <th>Min Temp</th>
+                        <th>Max Temp</th>
+                        <th>Wind</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {currentDayForecast.map((forecastField, index) => (
+                        <tr key={index}>
+                            <td colSpan="2">{forecastField.dt_txt}</td>
+                            <td>{forecastField.main.temp}</td>
+                            <td>{forecastField.main.temp_min}</td>
+                            <td>{forecastField.main.temp_max}</td>
+                            <td>{forecastField.wind.speed}</td>
+                            <td>{forecastField.weather[0].description}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+            <Pagination size="lg">{items}</Pagination>
+        </div>
     );
 };
 
